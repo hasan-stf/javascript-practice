ef3737840e388051c4f5952e3fd02a579f43ea07
hasan-stf <hasan8xp@gmail.com>
2026-02-11 05:03:46 +0600

    if else related

--- if.js
+++ if.js
@@ -3,13 +3,57 @@
 
 // if you buy food for me ,i will come
 //if biriyani price less than 300 taka i will buy 
+
+
+
+
+
 /* 
 
-if(){
-
+if( condition  ){
+//code to be executed
 }
 
 */
-if(3<10){
-    console.log
+
+if(13<10){
+    console.log("i have a smaller value");
 }
+
+// .............................................
+let a=2;
+if(a<=5){
+    console.log("if statement executed");
+}
+
+// ..............................................
+
+var biriyaniPrice =250;
+
+if(biriyaniPrice < 300){
+    console.log("mama give me some biriyani");
+}
+// output?
+
+//.................................................
+/* 
+
+if(condition){
+execute code if condition above is ture}
+
+else{
+  execute code if the condition is false  }
+
+*/
+
+var weight=40;
+if(weight<20){
+    console.log("ami niye ashbo");
+}
+else{
+    console.log("rickshaw vara korbo");
+}
+//output??
+
+//................................................
+
