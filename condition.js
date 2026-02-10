aa168a320a4b2de5e6dfb7f67364a27b4be18e30
hasan-stf <hasan8xp@gmail.com>
2026-02-11 01:36:32 +0600

    commented

--- condition.js
+++ condition.js
@@ -1,4 +1,4 @@
-console.log(5>10);
-console.log(10>5);
-console.log(10=='10');
-console.log(10==='10');
+console.log(5>10); //false
+console.log(10>5); //true
+console.log(10=='10'); //true
+console.log(10==='10'); //false
