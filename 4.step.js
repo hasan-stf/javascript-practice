844989c9964bdfa2468cd013e97b1ceaf2f2ef21
hasan-stf <hasan8xp@gmail.com>
2026-02-11 15:10:45 +0600

    again

--- 4.step.js
+++ 4.step.js
@@ -1,6 +1,7 @@
 //multi level condition
 
-const price=5000;
+/*
+ const price=5000;
 
 if(price>=5000){
     const discount=price*10/100;
@@ -8,3 +9,47 @@ if(price>=5000){
     const payAmount=price-discount;
     console.log(payAmount);
 }
+
+else{
+    console.log(price);
+} 
+     */
+   
+
+// .........................................
+
+if(){
+
+
+}
+
+else if(){
+
+}
+
+else(){
+
+
+}
+
+
+//...................................................
+
+const price=2000;
+
+if(price>=5000){
+    //10% discount
+    const discount=price*10/100;
+    const payAmount=price-discount;
+    console.log(payAmount);
+}
+else if(price>2500){
+    //5% discount
+    const discount=price*5/100;
+    const payAmount=price-discount;
+    console.log(payAmount)
+}
+else{
+    console.log(price);
+}
+
