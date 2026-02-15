ecb3c45e8626aa286cb48a34e79983b82bad2f05
hasan-stf <hasan8xp@gmail.com>
2026-02-15 14:44:45 +0600

    nested if else

---
+++ nested.js
@@ -0,0 +1,24 @@
+const money=0;
+//input : 500,200,20,0
+if(money>300){
+    console.log("bro you are rich");
+}
+
+else{
+
+
+if(money>100){
+    console.log("tui gorib o na , borolok o na ");
+}
+else{ 
+    if(money>0){
+        console.log("dosto kola kha  calcium bara");
+    }
+    else{
+        console.log("tui amar bondhu na");
+    }
+
+}
+
+
+}
