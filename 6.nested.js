868404ced955746cb48ba0baf0d3efcc78a437f5
hasan-stf <hasan8xp@gmail.com>
2026-02-15 14:45:52 +0600

    nested if else

--- 6.nested.js
+++ 6.nested.js
@@ -3,10 +3,7 @@ const money=0;
 if(money>300){
     console.log("bro you are rich");
 }
-
 else{
-
-
 if(money>100){
     console.log("tui gorib o na , borolok o na ");
 }
@@ -17,8 +14,5 @@ else{
     else{
         console.log("tui amar bondhu na");
     }
-
 }
-
-
 }
