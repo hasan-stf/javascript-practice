b4fcc56a3c27915b19a1254b659bf88213624a3b
hasan-stf <hasan8xp@gmail.com>
2026-02-16 00:22:47 +0600

    !!

---
+++ 8. logicalNotOperator.js
@@ -0,0 +1,21 @@
+const isLeader= true;
+
+//if(isLeader===true)
+// if(isLeader){
+// console.log("leader asche, chair charo");
+// }
+// else{
+//     console.log("vitore dukte dibe na");
+// }
+
+
+
+//.............................................................
+const isPassed =false;
+//if(isPassed===false)
+if(!isPassed){
+    console.log("tore biya diya dimu");
+}
+else{
+    console.log("tumi amar noyon er moni");
+}
